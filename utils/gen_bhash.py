1dad9c18bf8ab0c04f0d5d59a4dfe47d766dc6f2
Shadow <proteyrus@gmail.com>
2025-11-19 17:45:53 +0300

    Graphana dashboards

--- utils/gen_bhash.py
+++ utils/gen_bhash.py
@@ -1,2 +1,3 @@
+#python3 -c "import bcrypt; print(bcrypt.hashpw(b'my_prometheus_password', bcrypt.gensalt()).decode())"
 import bcrypt; 
 print(bcrypt.hashpw(b'my_prometheus_password', bcrypt.gensalt()).decode())
